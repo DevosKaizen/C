f65ca07d2a4287744c02a1dd4540e740e91dde13
AndrewDevos <andrew.cottadevos@gmail.com>
2023-09-20 17:07:03 -0300

    revoluçao farropilha

--- verificarSeePRIMO.c
+++ verificarSeePRIMO.c
@@ -3,13 +3,40 @@
 int main(){
 	
 	int x = 0;
+	int cont;
 	
 	//recebe os valores a calcular
-	printf("\nDado um inteiro positivo n, verificar se n ï¿½ primo.\n");
+	printf("\nDado um inteiro positivo n, verificar se n e primo.\n");
 	printf("Digite o numero base a ser Verificado:"); scanf("%d",&x);
 	printf("\n");
 	
 	int aux = x;
+	
+	if( x == 1){
+		printf("O numero %d sim e primo",x);
+	}
+	if( x == 2){
+		printf("O numero %d sim e primo",x);
+	}
+	if( x != 2 ; x % 2 == 0){
+		printf("O numero %d é par e nao e primo",x);
+	}
+	if( x != 3 ; x % 3 == 0){
+		printf("O numero %d nao e primo",x);
+	}
+	if( x != 3 ; x % 3 == 0){
+		printf("O numero %d é par e nao e primo",x);
+	}
+	
+	
+	
+	if( x % cont != 0; )
+	
+	
+	
+	
+	
+	
 	
 	if( x % 1 == 0 ){
 		
